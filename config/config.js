103f5f5baa353cad97dc00d5b5b2c5dcb524e726
Seb <sebastianjthomas@gmail.com>
2017-01-23 16:30:20 +0000

    Fixed iterator to save to db, added config, programmes population done

---
+++ config/config.js
@@ -0,0 +1,6 @@
+exports.config = {
+    bbcApi: {
+        base: 'http://www.bbc.co.uk/programmes/',
+        brandPids: ['b007qlvb', 'b006qsq5', 'b006qrpf', 'b006tp43', 'b006qp6p', 'b006qgj4', 'b006r9xr', 'b006tnsf']
+    }
+}
--- controllers/populate.js
+++ controllers/populate.js
@@ -1,14 +1,15 @@
 var Promise = require("bluebird");
 var rp = require('request-promise');
 var Programme = require('../models/programme');
+var config = require('../config/config');
 
 exports.populateProgrammes = function(req, res) {
-    const bbcPids = ['b007qlvb', 'DASDA', 'b006qrpf'];
+    const bbcPids = config.config.bbcApi.brandPids;
 
     const getResults = bbcPid => {
-        let url = 'http://www.bbc.co.uk/programmes/'+bbcPid+'.json';
+        let url = config.config.bbcApi.base+bbcPid+'.json';
 
-        return rp({uri: url, json: true})
+        return rp({uri: url, json: true, headers: {'User-Agent': 'Request-Promise'}})
             .then(result => ({result, success:true}))
             .catch(error => ({error, success:false}))
     }
@@ -18,52 +19,30 @@ exports.populateProgrammes = function(req, res) {
         .map(result => result.result);
 
     const findAndUpdate = results => {
-        const data = results.Object.programme;
-        
-        // Mongoose allows us query db for existing PID and upsert
-        const query = { pid: data.pid };
-        const update = {
-            name: data.title,
-            pid: data.pid,
-            desc: data.short_synopsis
-        };
-        const options = { upsert: true, new: true };
+        results.map(obj => {
+            const programme = obj.programme;
+            
+            // Mongoose allows us query db for existing PID and upsert
+            const query = { pid: programme.pid };
+            const update = {
+                name: programme.title,
+                pid: programme.pid,
+                desc: programme.short_synopsis
+            };
+            const options = { upsert: true, new: true };
 
-        return Programme.findOneAndUpdate(query, update, options)
+            // Programme.findOneAndUpdate(query, update, options)
+            return Programme.findOneAndUpdate(query, update, options, function(err, doc) {
+                if (err) return console.log(500, { error: err });
+                console.log("succesfully saved");
+            });
+        })
     }
 
     Promise
         .map(bbcPids, getResults)
         .then(filterSucceeded)
-    // .then(results => {
-    //     let succeeded = results
-    //         .filter(result => result.success)
-    //         .map(result => result.result);
-    //     let failed = results
-    //         .filter(result => !result.success)
-    //         .map(result => result.error);
-
-    //     console.log(results, succeeded, failed);
-    // })
-    .then(findAndUpdate)
-    .catch(console.log.bind(console))
-    .finally(function () {
-        // This is called after the request finishes either successful or not successful.
-        return res.json({ message: 'Programme updated! (maybe)' });
-    });
+        .then(findAndUpdate)
+        .catch(console.log.bind(console))
+        .finally(() => res.json({ message: 'Done' }));
 }
-
-// exports.hipsterJesus = {
-
-//     html: function(req, res) {
-//         return rp('http://hipsterjesus.com/api/').then(function(data) {
-//             res.json({message: data.text});
-//         });
-//     },
-
-//     paragraphs: function() {
-//         return this.html().then(function(html) {
-//             return html.replace(/<[^>]+>/g, "").split("");
-//         });
-//     }
-// };
